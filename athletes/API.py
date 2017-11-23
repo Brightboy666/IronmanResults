10bc290d3e9017618cc2c7f17c6b9babc13825eb
dado0583 <davidjdoherty@gmail.com>
2017-11-23 16:20:20 -0500

    Introducing a cache for accessing results

--- athletes/API.py
+++ athletes/API.py
@@ -1,52 +1,46 @@
-from athletes.AthleteLoader import get_results
+from athletes.AthleteLoader import Results
 from athletes.Results import Division
 import pandas as pd
 
+
+all_results = Results()
+
 def findAthleteResults(name:str, is_regex:bool = False):
-    results = []
+    df = all_results.get_results()
+    if is_regex:
+        df = df[df['name'].str.match(name)]
+    else:
+        df = df[df['name'].str.contains(name)]
 
-    for df in get_results():
-        if is_regex:
-            results.append(df[df['name'].str.match(name)])
-        else:
-            results.append(df[df['name'].str.contains(name)])
-
-    return pd.concat(results)
+    return df
 
 def findFastResults():
-    results = []
-
-    for df in get_results():
-        #We're removing athletes with slow times because we're not going to need to look at them
-        #for predicting who is worth watching at an upcoming race.
-        a = df.ix[((df['overallTime'] < pd.Timedelta('11:45:00')) & (df['overallTime'] > pd.Timedelta('07:20:00')) & (~df['raceName'].str.contains("70.3")))]
-        b = df.ix[((df['overallTime'] < pd.Timedelta('5:45:00')) & (df['overallTime'] > pd.Timedelta('3:20:00')) & (df['raceName'].str.contains("70.3")))]
+    df = all_results.get_results()
+    #We're removing athletes with slow times because we're not going to need to look at them
+    #for predicting who is worth watching at an upcoming race.
+    a = df.ix[((df['overallTime'] < pd.Timedelta('11:45:00')) & (df['overallTime'] > pd.Timedelta('07:20:00')) & (~df['raceName'].str.contains("70.3")))]
+    b = df.ix[((df['overallTime'] < pd.Timedelta('5:45:00')) & (df['overallTime'] > pd.Timedelta('3:20:00')) & (df['raceName'].str.contains("70.3")))]
     
-        results.append(a)
-        results.append(b)
-    return pd.concat(results)
+    return pd.concat([a,b])
 
 def findFastestResults(breach:int = 15):
-    results = []
+    df = all_results.get_results()
+    #Finding the fastest athlete's per race. We'll need to figure out how to separate out pros and non-pros
+    df = df.sort_values(by='overallTime', ascending=True).head(n=breach)
 
-    for df in get_results():
-        #Finding the fastest athlete's per race. We'll need to figure out how to separate out pros and non-pros
-        df = df.sort_values(by='overallTime', ascending=True).head(n=breach)
-        results.append(df)
+    return df
 
-    return pd.concat(results)
+def findResultsForAG(div:Division):
+    df = all_results.get_results()
+    #Finding the fastest athlete's per race. We'll need to figure out how to separate out pros and non-pros
+    df = df[df['currentAge'].between(div.lowAge, div.highAge)]
 
-def filter_by_ag(div:Division):
-    results = []
-
-    for df in get_results():
-        #Finding the fastest athlete's per race. We'll need to figure out how to separate out pros and non-pros
-        df = df[df['currentAge'].between(div.lowAge, div.highAge)]
-        results.append(df)
-
-    return pd.concat(results)
+    return df
 
 if __name__ == "__main__":
-    #print(findAthleteResults("Erika.*Sampaio", is_regex=True))
+    print(findAthleteResults("Erika.*Sampaio", is_regex=True))
+    print("***************")
+    print(findResultsForAG(Division(male=True, lowAge=35, highAge=39)))
+    print("***************")
     print(findFastestResults(breach=20))
     
--- athletes/AthleteLoader.py
+++ athletes/AthleteLoader.py
@@ -13,75 +13,86 @@ logger = logging.getLogger(__name__)
 
 import pandas as pd
 
-def get_results():
-    resultsDir = 'races/results'
-    files = (x for x in os.listdir(resultsDir) if (str(x).endswith(".csv")))# and ("70.3-a" in str(x))))
+class Results:
+    def __init__(self):
+        self.results_cache = None
+        tmp_cache = []
+        for df in self.__init_results__():
+            tmp_cache.append(df)
+        self.results_cache = pd.concat(tmp_cache)
 
-    master_df = None
+    def get_results(self):
+        return self.results_cache
 
-    for file in files:
+    def __init_results__(self):
+        resultsDir = 'races/results'
+        files = (x for x in os.listdir(resultsDir) if (str(x).endswith(".csv")))# and ("70.3-a" in str(x))))
 
-        if "5150" in file:
-            #For now we are going to ignore Olympic distance events
-            continue
+        master_df = None
 
-        try:
-            csv_location = "{}/{}".format(resultsDir, file)
+        for file in files:
 
-            #First row will be a description of the file
-            div_df = pd.DataFrame.from_csv(csv_location, header=1)
-
-            if(len(div_df) < 10):
-                logger.debug("{} likely to be an empty race".format(csv_location))
+            if "5150" in file:
+                #For now we are going to ignore Olympic distance events
                 continue
 
-            enrich_dataframe(div_df, file)
+            try:
+                csv_location = "{}/{}".format(resultsDir, file)
 
-            yield div_df
+                #First row will be a description of the file
+                div_df = pd.DataFrame.from_csv(csv_location, header=1)
+
+                if(len(div_df) < 10):
+                    logger.debug("{} likely to be an empty race".format(csv_location))
+                    continue
+
+                self.__enrich_dataframe__(div_df, file)
+
+                yield div_df
+            except Exception as e:
+                logger.exception("Had an issue with: {}".format(file))
+                logger.exception(e)
+
+    def __enrich_dataframe__(self, tmp_df, file):
+        #Setting times for these peeps as beng very slow
+        tmp_df.ix[tmp_df.overallTime.isin(['DNS', 'DNF', 'DQ', '---', pd.np.nan]), 'overallTime'] = "19:59:59"
+        tmp_df['overallTime'] = pd.to_timedelta(tmp_df['overallTime'])
+
+        #Changing values that aren't valid ages to 0 so they don't match
+        #the following filter
+        try:
+            tmp_df[['age']]
+
+            tmp_df.ix[tmp_df.age.isin(['---', pd.np.nan]), 'age'] = 0
+            tmp_df['age'].astype(int)
         except Exception as e:
-            logger.exception("Had an issue with: {}".format(file))
-            logger.exception(e)
-
-def enrich_dataframe(tmp_df, file):
-    #Setting times for these peeps as beng very slow
-    tmp_df.ix[tmp_df.overallTime.isin(['DNS', 'DNF', 'DQ', '---', pd.np.nan]), 'overallTime'] = "19:59:59"
-    tmp_df['overallTime'] = pd.to_timedelta(tmp_df['overallTime'])
-
-    #Changing values that aren't valid ages to 0 so they don't match
-    #the following filter
-    try:
-        tmp_df[['age']]
+            print("")
 
         tmp_df.ix[tmp_df.age.isin(['---', pd.np.nan]), 'age'] = 0
-        tmp_df['age'].astype(int)
-    except Exception as e:
-        print("")
+        tmp_df['age'] = tmp_df['age'].astype(int)
 
-    tmp_df.ix[tmp_df.age.isin(['---', pd.np.nan]), 'age'] = 0
-    tmp_df['age'] = tmp_df['age'].astype(int)
+        #We'll figure out the current age of each competitor.
+        #For example if someone was 24 when they raced two years ago
+        #They will now be 26 and we'll need this to know they are now
+        #in the 25-29 category, for example.
+        yrOfRace = int(re.match(".*-(\d\d\d\d)", file, re.IGNORECASE).group(1))
+        currentYr = datetime.datetime.today().year
+        yrsSinceRace = currentYr - yrOfRace
+        tmp_df['currentAge'] = tmp_df['age'] + yrsSinceRace
 
-    #We'll figure out the current age of each competitor.
-    #For example if someone was 24 when they raced two years ago
-    #They will now be 26 and we'll need this to know they are now
-    #in the 25-29 category, for example.
-    yrOfRace = int(re.match(".*-(\d\d\d\d)", file, re.IGNORECASE).group(1))
-    currentYr = datetime.datetime.today().year
-    yrsSinceRace = currentYr - yrOfRace
-    tmp_df['currentAge'] = tmp_df['age'] + yrsSinceRace
+        #Grabbing the date of the race from the file name in case we need it.
+        dateMatch = re.match(".*-(\d\d\d\d)(\d\d)(\d\d)", file, re.IGNORECASE)
+        raceDate = datetime.datetime(year=int(dateMatch.group(1)), month=int(dateMatch.group(2)), day=int(dateMatch.group(3)))
+        tmp_df['raceDate'] = raceDate
 
-    #Grabbing the date of the race from the file name in case we need it.
-    dateMatch = re.match(".*-(\d\d\d\d)(\d\d)(\d\d)", file, re.IGNORECASE)
-    raceDate = datetime.datetime(year=int(dateMatch.group(1)), month=int(dateMatch.group(2)), day=int(dateMatch.group(3)))
-    tmp_df['raceDate'] = raceDate
+        #Grabbing the race name from the file name
+        #We may want to change the structure of the files to have columns for
+        #race name, date and distance, although that would be a lot of 
+        #repeating data to go in the files...
+        raceMatch = re.match(".*?-(70\.3)?[-]?(.*)-.*", file, re.IGNORECASE)
+        raceName = "{}-{}".format(raceMatch.group(1), raceMatch.group(2)) if raceMatch.group(1) is not None else "{}".format(raceMatch.group(2))
+        tmp_df['raceName'] = raceName
 
-    #Grabbing the race name from the file name
-    #We may want to change the structure of the files to have columns for
-    #race name, date and distance, although that would be a lot of 
-    #repeating data to go in the files...
-    raceMatch = re.match(".*?-(70\.3)?[-]?(.*)-.*", file, re.IGNORECASE)
-    raceName = "{}-{}".format(raceMatch.group(1), raceMatch.group(2)) if raceMatch.group(1) is not None else "{}".format(raceMatch.group(2))
-    tmp_df['raceName'] = raceName
-
-    #Getting rid of the index which is the file name (we don't need it anyway)
-    tmp_df.reset_index(drop=True, inplace=True)
-    tmp_df.fillna("MISSIING DATA", inplace=True)
+        #Getting rid of the index which is the file name (we don't need it anyway)
+        tmp_df.reset_index(drop=True, inplace=True)
+        tmp_df.fillna("MISSIING DATA", inplace=True)
